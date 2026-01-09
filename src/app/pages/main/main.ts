40f5a5b20a9d61a1c826982f3008631471158913
arodovi <arodriguezo@iesrafaelalberti.es>
2026-01-09 12:27:36 +0100

    Main provisional terminado

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -33,38 +33,38 @@ export class Main {
     {
       id: 1,
       imageSrc: '/assets/Images_For_Card_1.jpg',
-      imageAlt: 'Serie 1',
-      hoverTitle: 'Stranger Things'
+      imageAlt: 'Twin Peaks',
+      hoverTitle: 'Twin Peaks'
     },
     {
       id: 2,
       imageSrc: '/assets/Images_For_Card_2.jpg',
-      imageAlt: 'Serie 2',
-      hoverTitle: 'Twin Peaks'
+      imageAlt: 'Stranger Things',
+      hoverTitle: 'Stranger Things'
     },
     {
       id: 3,
       imageSrc: '/assets/Image_For_Card_3.jpg',
-      imageAlt: 'Serie 3',
-      hoverTitle: 'Breaking Bad'
+      imageAlt: 'Alien: Earth',
+      hoverTitle: 'Alien: Earth'
     },
     {
       id: 4,
       imageSrc: '/assets/Image_For_Card_4.jpg',
-      imageAlt: 'Serie 4',
+      imageAlt: 'The Haunting of Hill House',
       hoverTitle: 'The Haunting of Hill House'
     },
     {
       id: 5,
       imageSrc: '/assets/Image_For_Card_5.jpg',
-      imageAlt: 'Serie 5',
+      imageAlt: 'The Walking Dead',
       hoverTitle: 'The Walking Dead'
     },
     {
       id: 6,
       imageSrc: '/assets/Image_For_Card_6.jpg',
-      imageAlt: 'Serie 6',
-      hoverTitle: 'Alien Earth'
+      imageAlt: 'Breaking Bad',
+      hoverTitle: 'Breaking Bad'
     }
   ];
 
@@ -75,38 +75,38 @@ export class Main {
     {
       id: 7,
       imageSrc: '/assets/Images_For_Card_7.png',
-      imageAlt: 'Serie 7',
-      hoverTitle: 'Stranger Things S5'
+      imageAlt: 'It: Welcome to Derry',
+      hoverTitle: 'It: Welcome to Derry'
     },
     {
       id: 8,
       imageSrc: '/assets/Images_For_Card_8.png',
-      imageAlt: 'Serie 8',
-      hoverTitle: 'Twin Peaks: The Return'
+      imageAlt: 'Buffy the Vampire Slayer',
+      hoverTitle: 'Buffy the Vampire Slayer'
     },
     {
       id: 9,
       imageSrc: '/assets/Images_For_Card_9.jpg',
-      imageAlt: 'Serie 9',
-      hoverTitle: 'Breaking Bad Movie'
+      imageAlt: 'Black Mirror',
+      hoverTitle: 'Black Mirror'
     },
     {
       id: 10,
       imageSrc: '/assets/Images_For_Card_10.jpg',
-      imageAlt: 'Serie 10',
-      hoverTitle: 'Hill House S2'
+      imageAlt: 'The Creep Tapes',
+      hoverTitle: 'The Creep Tapes'
     },
     {
       id: 11,
       imageSrc: '/assets/Images_For_Card_11.jpg',
-      imageAlt: 'Serie 11',
-      hoverTitle: 'Walking Dead: Origins'
+      imageAlt: 'Smiling Friends',
+      hoverTitle: 'Smiling Friends'
     },
     {
       id: 12,
       imageSrc: '/assets/Images_For_Card_12.jpg',
-      imageAlt: 'Serie 12',
-      hoverTitle: 'Alien Earth: Prologue'
+      imageAlt: "JoJo's Bizarre Adventure",
+      hoverTitle: "JoJo's Bizarre Adventure"
     }
   ];
 
